7a94bdf9956b16214069ae65a57ece545cb667df
ellenclynch@gmail.com <ellenlynch@ellens-mbp.attlocal.net>
2019-07-08 08:38:32 -0400

    creating new stranger things web site

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import en_US from '../assets/en_US.json';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'friday-stranger-things';
+  title = en_US.heading;
+  subhead = en_US.description;
+  snippetQuotes = en_US.snippets;
+  locations = en_US.locations;
+  video = en_US['video-embed'];
+  quoteText = en_US.quote.text;
+  quoteAuthor = en_US.quote.author;
+  
+  
+  constructor(){
+    console.log(en_US.gallery[0])
+  }
 }
